85dd7929e919dfc95d4de5d83146db3fcd633b60
Joshua Sproule <joshsproule@live.co.uk>
2025-02-19 01:09:16 +0000

    Style: Edit User Page

--- src/pages/Users/UserFormPage.tsx
+++ src/pages/Users/UserFormPage.tsx
@@ -1,5 +1,8 @@
-import { useLocation } from 'react-router';
+import { Link, useLocation } from 'react-router';
 import UserForm from './UserForm';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { FaArrowLeft } from 'react-icons/fa6';
 
 
 
@@ -7,12 +10,23 @@ function UserFormPage() {
 
     const location = useLocation();
     return (
-        <div className="flex-grow flex-col bg-stone-50 dark:bg-stone-900">
-            <h1 className="text-black dark:text-white text-2xl mx-10 my-6 font-semibold ">{location.pathname.includes("add") ? "Add a new " : "Update the"} User</h1>
-            <div className='md:w-8/12'>
-                <UserForm />
+        <div className="md:p-6 flex flex-col flex-grow md:mx-10">
+            <Link to="/users" className='w-fit'>
+                <Button variant={"link"}>
+                    <FaArrowLeft />
+                    Back
+                </Button>
+            </Link>
+            <div className="flex flex-col mx-auto mb-10">
+                <Card className="w-svw sm:w-[28rem]">
+                    <CardHeader>
+                        <CardTitle>{location.pathname.includes("add") ? "Add a new " : "Update the"} User</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <UserForm />
+                    </CardContent>
+                </Card>
             </div>
-
         </div>
     )
 }
